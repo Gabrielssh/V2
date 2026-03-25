8982b331798faecd56ee29f6129b28a319769f9f
Gabrielssh <139812368+Gabrielssh@users.noreply.github.com>
2026-03-25 00:32:20 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 apt update
 apt install unrar -y
-wget https://github.com/sibuk76/o11-OTT-v2.2b1/raw/main/o11.rar
+wget https://github.com/Gabrielssh/V2/blob/main/o11.rar
 unrar x o11.rar /home
 chmod -R +x /home/o11/
-wget https://raw.githubusercontent.com/sibuk76/o11-OTT-v2.2b1/main/o11.service
+wget https://raw.githubusercontent.com/Gabrielssh/V2/refs/heads/main/o11.service
 chmod +x o11.service
 mv ./o11.service /etc/systemd/system/
 systemctl daemon-reload
